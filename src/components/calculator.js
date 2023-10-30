2ecb7e7ff69beacc4d61618d81c9a350d06a19b4
ritika-jhanwar <ritika.jhanwar10@gmail.com>
2023-10-30 19:04:49 +0530

    css done

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -5,23 +5,28 @@ export default function Calculator() {
   return (
     <div className="container">
       <div className="calculator">
+        <div className="top">
+          <div className="circle"></div>
+        </div>
+        <div className="display"></div>
+        <div className="extra"></div>
         <div className="keyboard">
-          <div className="grid-item">7</div>
-          <div className="grid-item">8</div>
-          <div className="grid-item">9</div>
+          <div className="grid-item nums">7</div>
+          <div className="grid-item nums">8</div>
+          <div className="grid-item nums">9</div>
           <div className="grid-item">⌫</div>
-          <div className="grid-item">4</div>
-          <div className="grid-item">5</div>
-          <div className="grid-item">6</div>
-          <div className="grid-item">÷</div>
-          <div className="grid-item">1</div>
-          <div className="grid-item">2</div>
-          <div className="grid-item">3</div>
+          <div className="grid-item nums">4</div>
+          <div className="grid-item nums">5</div>
+          <div className="grid-item nums">6</div>
+          <div className="grid-item divide">÷</div>
+          <div className="grid-item nums">1</div>
+          <div className="grid-item nums">2</div>
+          <div className="grid-item nums">3</div>
           <div className="grid-item">-</div>
-          <div className="grid-item">0</div>
+          <div className="grid-item bot-left">0</div>
           <div className="grid-item">.</div>
           <div className="grid-item">=</div>
-          <div className="grid-item">+</div>
+          <div className="grid-item bot-right">+</div>
         </div>
       </div>
     </div>
