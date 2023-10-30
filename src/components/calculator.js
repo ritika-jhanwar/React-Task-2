26a7d6ccc4214f63a10c012e6306edd756a30cf6
ritika-jhanwar <ritika.jhanwar10@gmail.com>
2023-10-30 18:36:01 +0530

    Basic html css done

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,21 +1,29 @@
-import React from 'react'
+import React from "react";
+import "./calc.css";
 
 export default function Calculator() {
-    const containerStyle={
-        background: 'linear-gradient(274deg, rgba(2,0,36,1) 0%, rgba(25,207,138,1) 0%, rgba(23,208,125,1) 40%, rgba(17,205,209,1) 61%, rgba(0,212,255,1) 100%)',
-        height:'100vh',
-        display:'flex',
-        justifyContent:'center',
-        alignItems:'center'
-    }
-    const calcBody={
-        height:'70%',
-        width:'25%',
-        backgroundColor:'blue'
-    }
   return (
-    <div className='container' style={containerStyle}>
-      <div className="calculator" style={calcBody}></div>
+    <div className="container">
+      <div className="calculator">
+        <div className="keyboard">
+          <div className="grid-item">7</div>
+          <div className="grid-item">8</div>
+          <div className="grid-item">9</div>
+          <div className="grid-item">⌫</div>
+          <div className="grid-item">4</div>
+          <div className="grid-item">5</div>
+          <div className="grid-item">6</div>
+          <div className="grid-item">÷</div>
+          <div className="grid-item">1</div>
+          <div className="grid-item">2</div>
+          <div className="grid-item">3</div>
+          <div className="grid-item">-</div>
+          <div className="grid-item">0</div>
+          <div className="grid-item">.</div>
+          <div className="grid-item">=</div>
+          <div className="grid-item">+</div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
